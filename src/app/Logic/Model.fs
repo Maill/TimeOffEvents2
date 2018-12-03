2d400fc7eed134debc7c57c6abca08386b8e1bce
Nicolas Cornu <nicolas.demoncourt@gmail.com>
2018-12-03 20:05:47 +0100

    OverlapsWith implementation

--- src/app/Logic/Model.fs
+++ src/app/Logic/Model.fs
@@ -51,11 +51,20 @@ module Logic =
         let newRequestState = evolveRequest requestState event
         userRequests.Add (event.Request.RequestId, newRequestState)
 
-    let overlapsWith request1 request2 =
-        false //TODO: write a function that checks if 2 requests overlap
+    let isContainedIn baseRequest comparedRequest = 
+        comparedRequest.End < baseRequest.Start || baseRequest.End < comparedRequest.Start
 
+    let overlapsWith request1 request2 =
+        request1.Start = request2.Start 
+        || request1.End = request2.End
+        || request1.End = request2.Start
+        || request1.Start = request2.End
+        || request1 |> isContainedIn request2 
+        || request2 |> isContainedIn request1
+    
     let overlapsWithAnyRequest (otherRequests: TimeOffRequest seq) request =
-        false //TODO: write this function using overlapsWith
+        otherRequests
+        |> Seq.exists (overlapsWith request)
 
     let createRequest activeUserRequests  request =
         if request |> overlapsWithAnyRequest activeUserRequests then
